6ddc5c6f4746fa4e7b46c43abc469757d0b49183
Rachelle <rrathbone@atlassian.com>
2023-02-28 16:31:59 +1100

    initial commit

---
+++ src/main/java/com/rachellerathbone/lil/learningspring/data/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.rachellerathbone.lil.learningspring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
